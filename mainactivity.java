2ea99ddff61a5784e582f49bfb802312ae69debe
bsef18a514 <91655339+bsef18a514@users.noreply.github.com>
2021-12-06 21:11:59 +0500

    display changes

--- mainactivity.java
+++ mainactivity.java
@@ -1,3 +1,17 @@
+package com.android.appbar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.Button;
+import android.view.ViewGroup;
+
+public class MainActivity extends AppCompatActivity
 @Override
 protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
