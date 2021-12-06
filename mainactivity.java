8489bb8dcd7adadb1cfc2aebb8ead67eb0d33654
bsef18a514 <91655339+bsef18a514@users.noreply.github.com>
2021-12-06 21:13:12 +0500

    name changed

--- mainactivity.java
+++ mainactivity.java
@@ -1,7 +1,7 @@
 package com.android.appbar;
 
 import android.support.v7.app.AppCompatActivity;
-
+import android.os.Bundle;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
